eeca05f0529dc0aad49cfc31277c5c2eff7e483a
Joshua Arnott <josh@snorfalorpagus.net>
2020-04-07 01:41:10 +0100

    Improved tests for soil storage

--- src/soil.rs
+++ src/soil.rs
@@ -68,29 +68,75 @@ mod tests {
     use super::*;
 
     #[test]
-    fn zero_storage() {
+    fn drying() {
+        // When PET is greater than rainfall the effective rainfall is
+        // negative and drying occurs
         let mut soil = SoilMoistureDeficitStore {
             direct_percolation: 1.0,
-            potential_drying_constant: 0.0,
-            gradient_drying_curve: 0.0,
+            potential_drying_constant: 70.0,
+            gradient_drying_curve: 0.5,
             upper_deficit: 0.0,
             lower_deficit: 0.0,
         };
-        let percolation = soil.step(50.0, 15.0);
-        assert!(abs_diff_eq!(percolation, 35.0, epsilon = 0.0001))
+        // The upper storage dries first
+        let percolation = soil.step(50.0, 100.0);
+        assert!(abs_diff_eq!(percolation, 0.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 50.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 0.0, epsilon = 0.0001));
+        // Once the threshold of the upper storage is reached the lower storage
+        // begins to dry, at a rate defined by the gradient drying curve
+        let percolation = soil.step(50.0, 100.0);
+        assert!(abs_diff_eq!(percolation, 0.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 70.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 15.0, epsilon = 0.0001));
     }
 
     #[test]
-    fn negative_effective_rainfall() {
-        // When PET > rainfall there is zero percolation
+    fn wetting() {
+        let mut soil = SoilMoistureDeficitStore {
+            direct_percolation: 0.0,
+            potential_drying_constant: 15.0,
+            gradient_drying_curve: 1.0,
+            upper_deficit: 15.0,
+            lower_deficit: 15.0,
+        };
+        // Upper deficit is satisfied first
+        let percolation = soil.step(10.0, 0.0);
+        assert!(abs_diff_eq!(percolation, 0.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 5.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 15.0, epsilon = 0.0001));
+        // Lower deficit is satisfied second
+        let percolation = soil.step(10.0, 0.0);
+        assert!(abs_diff_eq!(percolation, 0.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 0.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 10.0, epsilon = 0.0001));
+        // Remaining flow after satisfying deficit percolates
+        let percolation = soil.step(15.0, 0.0);
+        assert!(abs_diff_eq!(percolation, 5.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 0.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 0.0, epsilon = 0.0001));
+    }
+
+    #[test]
+    fn direct_percolation_bypasses_stores() {
         let mut soil = SoilMoistureDeficitStore {
             direct_percolation: 1.0,
-            potential_drying_constant: 0.0,
-            gradient_drying_curve: 0.0,
-            upper_deficit: 0.0,
-            lower_deficit: 0.0,
+            potential_drying_constant: 100.0,
+            gradient_drying_curve: 1.0,
+            upper_deficit: 100.0,
+            lower_deficit: 100.0,
         };
-        let percolation = soil.step(50.0, 100.0);
-        assert!(abs_diff_eq!(percolation, 0.0, epsilon = 0.0001))
+        // When direct percolation is 1 all of the rainfall bypasses the store
+        let percolation = soil.step(50.0, 0.0);
+        assert!(abs_diff_eq!(percolation, 50.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 100.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 100.0, epsilon = 0.0001));
+        // When direct percolation is 0.5, half bypasses the store while the
+        // other half must first satisfy the deficit
+        soil.direct_percolation = 0.5;
+        let percolation = soil.step(50.0, 0.0);
+        assert!(abs_diff_eq!(percolation, 25.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.upper_deficit, 75.0, epsilon = 0.0001));
+        assert!(abs_diff_eq!(soil.lower_deficit, 100.0, epsilon = 0.0001));
     }
 }
